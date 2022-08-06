eb0ecfd84761e9c5ec49da3f76ff554786332a43
YounesAEO <y.aboueleomoum09@gmail.com>
2022-08-06 17:40:17 +0200

    added cta component in header

---
+++ src/components/header/Cta.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Cta = () => {
+	return (
+		<div className="cta">
+			<a href="../../assets/cv.pdf" download className="btn">
+				Download CV
+			</a>
+			<a href="#contact" className="btn btn-primary">
+				{' '}
+				Let's Talk
+			</a>
+		</div>
+	);
+};
+
+export default Cta;
