3a48a20926a9fe4149b5f453d91ca09982119fe5
YounesAEO <y.aboueleomoum09@gmail.com>
2022-08-07 21:29:38 +0200

    style header and add header socials compoenent

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './header.css';
+import Cta from './Cta';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header>
+			<div className="container header-container">
+				<h5>Hello I'm</h5>
+				<h1>Younes ABOU EL OMOUM</h1>
+				<h5 className="text-light"> FullStack Developer & AI Nerd</h5>
+				<Cta />
+				<HeaderSocials />
+				<div className="my-pic">
+					<img src={require('../../assets/me.png')} alt="my pic" />
+				</div>
+				<a href="#contact" className="scroll-down">
+					Scroll Down
+				</a>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BsLinkedin, BsDribbble, BsGithub } from 'react-icons/bs';
+
+const HeaderSocials = () => {
+	return (
+		<div className="header-socials">
+			<a href="https://linkedin.com" target="_blank">
+				<BsLinkedin />
+			</a>
+			<a href="https://github.com" target="_blank">
+				{' '}
+				<BsGithub />
+			</a>
+			<a href="https://dribble.com" target="_blank">
+				<BsDribbble />
+			</a>
+		</div>
+	);
+};
+
+export default HeaderSocials;
