b79fcff9ce79cc25ef2fcb29723356b7145de5b9
YounesAEO <y.aboueleomoum09@gmail.com>
2022-08-12 19:15:57 +0200

    added content to footer section and styled it

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import './footer.css';
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { IoLogoTwitter } from 'react-icons/io';
 
 const Footer = () => {
-	return <section id="footer">Footer</section>;
+	return (
+		<footer id="footer">
+			<a href="#" className="footer-logo">
+				YounesAEO
+			</a>
+			<ul>
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#about">About</a>
+				</li>
+				<li>
+					<a href="#experience">Experience</a>
+				</li>
+				<li>
+					<a href="#services">Services</a>
+				</li>
+				<li>
+					<a href="#portfolio">Portfolio</a>
+				</li>
+				<li>
+					<a href="#testimonials">Testimonials</a>
+				</li>
+				<li>
+					<a href="#contact">Contact</a>
+				</li>
+				<div className="footer-socials">
+					<a href="https://facebook.com">
+						<FaFacebookF />
+					</a>
+					<a href="https://instagram.com">
+						<FiInstagram />
+					</a>
+					<a href="https://twitter.com">
+						<IoLogoTwitter />
+					</a>
+				</div>
+
+				<div className="footer-copyrights">
+					<small>&copy; Younes AEO. All rights reserved. </small>
+				</div>
+			</ul>
+		</footer>
+	);
 };
 
 export default Footer;
