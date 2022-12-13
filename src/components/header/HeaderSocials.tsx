339a386bf1c825fb147e304ac4a2204d46e6ad99
YounesAEO <y.aboueleomoum09@gmail.com>
2022-12-13 16:20:21 +0100

    add missing social media links

--- src/components/header/HeaderSocials.tsx
+++ src/components/header/HeaderSocials.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { BsLinkedin, BsDribbble, BsGithub } from 'react-icons/bs';
+import { BsLinkedin, BsDribbble, BsGithub, BsTwitter } from 'react-icons/bs';
 
 const HeaderSocials = () => {
 	return (
 		<div className="header-socials">
-			<a href="https://linkedin.com" target="_blank" rel="noreferrer">
+			<a
+				href="https://www.linkedin.com/in/younes-abou/"
+				target="_blank"
+				rel="noreferrer">
 				<BsLinkedin />
 			</a>
-			<a href="https://github.com" target="_blank" rel="noreferrer">
+			<a
+				href="https://github.com/YounesAEO"
+				target="_blank"
+				rel="noreferrer">
 				{' '}
 				<BsGithub />
 			</a>
-			<a href="https://dribble.com" target="_blank" rel="noreferrer">
-				<BsDribbble />
+			<a
+				href="https://twitter.com/YAbouelomoum"
+				target="_blank"
+				rel="noreferrer">
+				<BsTwitter />
 			</a>
 		</div>
 	);
