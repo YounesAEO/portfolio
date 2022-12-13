ed0449689947e5afed8161079630d44de20ae56e
YounesAEO <y.aboueleomoum09@gmail.com>
2022-12-13 19:51:58 +0100

    change ip collection api

--- src/utils/geo.ts
+++ src/utils/geo.ts
@@ -4,7 +4,5 @@ export function getLocationFromIp() {
 	// return axios.get(
 	// 	`http://api.ipstack.com/1.32.239.255?access_key=${process.env.REACT_APP_IPSTACK_API_KEY}&fields=country_code,region_code,city`
 	// );
-	return axios.get(
-		`http://api.ipstack.com/check?access_key=${process.env.REACT_APP_IPSTACK_API_KEY}&fields=country_code,region_code,city`
-	);
+	return axios.get(`https://www.cloudflare.com/cdn-cgi/trace`);
 }
