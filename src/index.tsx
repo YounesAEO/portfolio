4a65d22a03401a0845127dacb892206d1bdcb1dc
YounesAEO <y.aboueleomoum09@gmail.com>
2022-12-29 23:07:33 +0100

    fix for deployment problem with gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+	createBrowserRouter,
+	createHashRouter,
+	RouterProvider,
+} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { TiArrowBack } from 'react-icons/ti';
 import App from './App';
@@ -9,6 +13,13 @@ import Motivation from './Motivation';
 import './index.css';
 
 const route = createBrowserRouter([
+	{ path: '/portfolio', element: <App /> },
+	{ path: '/personal', element: <App2 /> },
+	{
+		path: '/motivation/:target',
+		element: <Motivation />,
+		loader: ({ params }) => params.target,
+	},
 	{
 		path: '/*',
 		element: (
@@ -20,13 +31,6 @@ const route = createBrowserRouter([
 				</Link>
 			</div>
 		),
-	},
-	{ path: '/personal', element: <App2 /> },
-	{ path: '/portfolio', element: <App /> },
-	{
-		path: '/motivation/:target',
-		element: <Motivation />,
-		loader: ({ params }) => params.target,
 	},
 ]);
 
